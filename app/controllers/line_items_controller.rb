fc041460c687b536ac25e9ff232fc4e5a06a6082
jeremylinlin <linlin_19930805@yahoo.com>
2014-09-05 13:01:34 +0800

    Created a cart

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,77 @@
+class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]  # This line makes sure that a cart is created when the "Add to cart" button is pressed.
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy] # The generated controller already uses this facility to set the value of the @line_item instance variable before the show() , edit() , update(), or destroy() actions are called.
+
+  # GET /line_items
+  # GET /line_items.json
+  def index
+    @line_items = LineItem.all
+  end
+
+  # GET /line_items/1
+  # GET /line_items/1.json
+  def show
+  end
+
+  # GET /line_items/new
+  def new
+    @line_item = LineItem.new
+  end
+
+  # GET /line_items/1/edit
+  def edit
+  end
+
+  # POST /line_items
+  # POST /line_items.json
+  def create
+    product = Product.find(params[:product_id]) # use the params object to get the :product_id parameter from the request
+    @line_item = @cart.line_items.build(product: product) # This causes a new line item relationship to be built between the @cart object and the product. You can build the relationship from either end, and Rails will take care of establishing the connections on both sides. We save the resulting line item into an instance variable named @line_item. NOT FULLY UNDERSTOOD!
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /line_items/1
+  # PATCH/PUT /line_items/1.json
+  def update
+    respond_to do |format|
+      if @line_item.update(line_item_params)
+        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @line_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /line_items/1
+  # DELETE /line_items/1.json
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_line_item
+      @line_item = LineItem.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def line_item_params
+      params.require(:line_item).permit(:product_id, :cart_id)
+    end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :price, :presence => true
 	validates :title, :uniqueness => true,
 					  :length => { minimum: 10 }
@@ -12,4 +16,16 @@ class Product < ActiveRecord::Base
 		Product.order(:updated_at).last  # Defined for the caching in store index
 	end
 
+	private
+
+		# ensure that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
+		
 end

bf1973681ca7909c58eb84321d2422c9f179b1e5
jeremylinlin <linlin_19930805@yahoo.com>
2014-09-05 08:43:48 +0800

    Validations & Tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
-	# validates :title, :presence => true
+	validates :title, :description, :price, :presence => true
+	validates :title, :uniqueness => true,
+					  :length => { minimum: 10 }
+	validates :image_url, allow_blank: true, :format => {
+		with: %r{\.(gif|jpg|png)\Z}i,	# but it's okay if blank
+		message: 'must be a URL for GIF, JPG or PNG image'
+	}
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end

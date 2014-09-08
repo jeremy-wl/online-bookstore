f55f97a35ba9c9b9f9478654672ab4c40e39ce9e
jeremylinlin <linlin_19930805@yahoo.com>
2014-09-08 12:25:05 +0800

    Update the config file for the production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+         address:              "smtp.gmail.com",
+         port:                  587,
+         domain:                "online-bookstore.herokuapp.com",
+         authentication:        "plain",
+         user_name:             "linlin.huangwenlin",
+         password:              "huangwenlin0805",
+         enable_starttls_auto:  true
+  }
+  
 end

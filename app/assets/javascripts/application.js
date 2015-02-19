9c6feb48a356a28b40b6ed184923e20ebb27aaec
jeremylinlin <linlin_19930805@yahoo.com>
2015-02-19 23:38:17 +0800

    Sign in using Bootstrap modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,7 @@ jQuery.expr[':'].contains = function(a, i, m) {
     return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
 var toggleActiveClass = function() {
-    $("#columns li").hover(function() {
+    $(".columns li:not('#sum')").hover(function() {
         $(this).toggleClass("active");
     });
 };
@@ -42,6 +42,8 @@ $(function() {
     // $(".navbar-default .navbar-nav > li>a:contains('My Cart')")
     toggleActiveClass();
     toggleCartOnMouseEvents();
+    $('[data-toggle="tooltip"]').tooltip()
+
 })
 
 // This fixes the $(document).ready() not loaded issue caused by turbolink
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include StoreHelper
+
   protected
 
     def set_i18n_locale_from_params
